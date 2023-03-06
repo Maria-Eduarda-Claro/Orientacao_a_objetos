d7c4f966426e058df507c6cefd3be1c16f633cef
Maria Eduarda Claro <79665660+Maria-Eduarda-Claro@users.noreply.github.com>
2023-03-06 20:40:06 -0300

    Add files via upload
    Primeiro contato com JAVA, iniciações básicas, feitas ainda no bloco de notas

---
+++ materias.java
@@ -0,0 +1,20 @@
+class materias{
+	public static void main(String args[]){
+		 System.out.println("Prof. Fabio: Banco de dados");
+		 System.out.println("Prof. Cleiton: Interfaces dinamicas");
+		 System.out.println("Prof. Riedi: Redes");
+		 System.out.println("Prof. Denis: Orientacao a objetos");
+		 System.out.println("Prof. Lucas: Robotica");
+		 System.out.println("Esta eh a grade do 3 periodo de ADS!");
+		 System.out.println("The end...");
+
+
+
+
+
+
+
+
+
+	}
+}
